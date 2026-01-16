174d0facb734bc12e2246f0bf7b06f95c491649a
1KoT1 <vitya.bobrov.16@gmail.com>
2026-01-16 12:06:51 +0500

    fix post code

--- static/post_decode.js
+++ static/post_decode.js
@@ -1,13 +1,17 @@
 document.getElementById('reverse_minification').addEventListener('submit', async (event) => {
     event.preventDefault();
 
+    if (!window.selectedFile) {
+        alert('Файл не выбран!');
+        return;
+    }
+
     const formData = new FormData();
-    // Добавляем данные
-    formData.append('filename', document.getElementById('fileList').value);
+    formData.append('filename', window.selectedFile);
     formData.append('code', document.getElementById('fileMessage').innerText);
 
     await fetch('/view_analysis_api/reverse_minification', {
         method: 'POST',
-        body: formData // Отправляем как форму, а не JSON
+        body: formData
     });
 });
